e83104ccbf68e3e4542bcbeb25bfc70a6fa0e7e9
FatemaSaifee <fatema.saifee711@github.com>
2014-08-16 11:12:45 +0530

    remove duplicates added

---
+++ 5.2.2-remove-duplicates.py
@@ -0,0 +1,24 @@
+""" Given a randomly ordered set of n integers,
+sort them into non-descending order using the selection sort.
+"""
+def selectionsort(a, n):
+    i = 0 #first element in unsorted part of array
+    j = 0 #index of unsorted part of array
+    p = 0 #position of min
+    min = 0 #current minimum
+    
+    assert n > 0
+    a = list(set(a))
+    n = len(a)
+    for i in range(0,n-1):
+        min = a[i]
+        p = i
+        for j in range(i+1 , n):
+            if a[j] == min:
+                a.remove(a[j])
+            if a[j] < min:
+                min = a[j]
+                p = j
+        a[p] = a[i]
+        a[i] = min
+    return a
